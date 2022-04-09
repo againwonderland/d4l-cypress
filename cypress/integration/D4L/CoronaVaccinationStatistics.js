9f81d116f8ebf3305d9ce7984c9d151276532850
againwonderland <agnieszka.krajewska01@gmail.com>
2022-04-09 21:12:28 +0200

    Save progress

--- cypress/integration/D4L/CoronaVaccinationStatistics.js
+++ cypress/integration/D4L/CoronaVaccinationStatistics.js
@@ -15,15 +15,20 @@ describe("Validate Vaccinations statistics for Germany", () => {
         cy.clearCookies();
         cy.clearLocalStorage();
         cy.visit('/en/vaccine/corona-vaccine/');
+
         cy.get('[data-corona-number="today_deutschland_vaccinated"]').as('totalVaccinations')
         cy.get('[data-corona-number="diff_deutschland_vaccinated"]').as('totalVaccinationsYesterday')
         cy.get('[data-corona-number="today_deutschland_vaccinated_to_total_perc"]').as('percentTotalVaccinations')
         cy.get('[data-corona-number="today_deutschland_vaccination_updated"]').as('TimestampVaccinationUpdate')
+        cy.get('.coronaStatistics__info > h2').as('coronaStatisticsHeader')
+        cy.get('.select__label').as('selectLand')
+        cy.get('[data-corona-number="today_deutschland_vaccination_updated"]').as('coronaStatisticsTextSmall')
+        cy.get('.coronaStatistics__text small').as('textSmall')
         // click banner to make more ov the page visible
         cy.get('.js-🍪-accept').click();
     })
 
-    it("First total should be a reasonable number", () => {
+    it("First total of vaccinated people should be a reasonable number", () => {
         
         cy.get('.select__element').select(0);
         cy.get('@numbers').then( (numbers) => {
@@ -87,7 +92,7 @@ describe("Validate Vaccinations statistics for Germany", () => {
         cy.get('@percentTotalVaccinations').then(() => {
             let numFirstPercent = 0
             cy.get('@percentTotalVaccinations').each(($item, $index, $list) => {
-                let itemIntPercent = parseInt($item.text().replaceAll('%',''))
+                let itemIntPercent = parseInt($item.text().replaceAll('%','').replaceAll('.',''))
                 if ($index == 0) {
                     numFirstPercent = itemIntPercent
                 } else {
@@ -97,7 +102,7 @@ describe("Validate Vaccinations statistics for Germany", () => {
         })
     });
 
-   it.only("Date of information is today", () => {
+   it("Date of information is today (passes when today is a workday)", () => {
        cy.get('@TimestampVaccinationUpdate').invoke('text').
        then((text) => {
            //retrieve current date and format as on the page
@@ -107,8 +112,47 @@ describe("Validate Vaccinations statistics for Germany", () => {
 
    });
 
-    it("Totals are the same when user switches the language to German")
-    it("Po angielsku jest po angielsku, po niemiecku jest po niemiecku")
-    it("User moze wybrac rozne miasta z listy")
+    it("Texts are displayed in correct language (test of chosen texts)", () => {
+        cy.get('@coronaStatisticsHeader').invoke('text').should('include', ("Vaccinations statistics for Germany"))
+        cy.get('@selectLand').invoke('text').should('include', ("Please select"))
+        cy.get('@textSmall').invoke('text').should('include', ("Current vaccines require two shots"))
+        cy.visit('/de/impfung/corona-impfung/')
+        cy.get('@coronaStatisticsHeader').invoke('text').should('include', ("Impfstatistik für Deutschland"))
+        cy.get('@selectLand').invoke('text').should('include', ("Bitte wählen"))
+        cy.get('@textSmall').invoke('text').should('include', ("Die aktuellen Impfstoffe erfordern eine Zweifachimpfung"))
+        //cy.visit('/en/vaccine/corona-vaccine/');
+    });
+
+    it.only("Totals are the same when user switches the language to German", () => {
+        cy.get('.select__element').select(0);
+        cy.get('@totalVaccinations').first().invoke('text').
+            then( $txt => $txt.replaceAll(',','')).
+            then(parseInt).
+            as('totalVaccinationsEN');
+
+       cy.visit('/de/impfung/corona-impfung/');
+       cy.get('.select__element').select(0);
+       
+       cy.get('@totalVaccinations').first().invoke('text').
+           then( ($txt) => $txt.replaceAll('.','')).
+           then(parseInt).
+           then((numberDE) => {
+               cy.get('@totalVaccinationsEN').should('be.eq',numberDE)
+           });       
+    });
+
+    it("When user selects Land, total of vaccinated people gets changed", () => {
+        cy.get('.select__element').select(3);
+        cy.get('[data-corona-number="today_berlin_vaccinated"]').as('totalVaccinationsBerlin')
+        cy.get('@numbers').then( (numbers) => {
+            cy.get('@totalVaccinationsBerlin').first().invoke('text').
+                then( $txt => $txt.replaceAll(',','')).
+                then(parseInt).
+                should('be.greaterThan', -1).
+                and('be.lessThan', numbers.numberOfPeopleInBerlin) 
+        });        
+
+    });
+
     it("A11Y test")
 }); 
